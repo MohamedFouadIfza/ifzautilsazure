511275307d2ca387c9448529f449db721bc2e956
Mohamed Fouad Ibrahim Mohamed Abdelkhalek <mabdelkhalek@ifza.com>
2024-08-22 16:56:50 +0400

    fix limit

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use('/users', usersRouter);
 
 
 app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb', parameterLimit: 50000 }));
 
 
 app.use('/api', MedicalTest);
@@ -35,12 +35,12 @@ app.use('/api', Passport);
 app.use('/api', Portal);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
