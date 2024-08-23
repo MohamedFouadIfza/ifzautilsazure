56ac0aae1a4d9a8795e45dd2b83041b2d726ae3d
Mohamed Fouad Ibrahim Mohamed Abdelkhalek <mabdelkhalek@ifza.com>
2024-08-23 10:48:40 +0400

    add catch block

--- API/passport.js
+++ API/passport.js
@@ -6,58 +6,66 @@ const fs = require('fs');
 const { sendPassport, fireOCR, getApplicant } = require('../utils/sumSub');
 
 router.post(`/${APINAME}/ocr`, async (req, res) => {
-    const {
-        applicantId,
-        base64,
-        fileName,
-        country,
-        externalUserId,
-        secretKey,
-        token
-    } = req.body
-
-    const fileDIr = path.join(__dirname, "../", "resources");
-
-    fs.writeFile(`${fileDIr}/${fileName}`, base64, { encoding: "base64" }, async (err) => {
-        if (err) {
-            console.log("errr", err)
-            res.status(400).json({
-                err
-            })
-            return
-        }
-
-
-        await sendPassport(applicantId, `${fileDIr}/${fileName}`, {
+    try {
+        const {
+            applicantId,
+            base64,
+            fileName,
             country,
-            idDocType: "PASSPORT"
-        }, secretKey, token)
+            externalUserId,
+            secretKey,
+            token
+        } = req.body
 
-        await fireOCR(applicantId, secretKey, token)
-        setTimeout(async () => {
-            getApplicant(externalUserId, secretKey, token).then((app) => {
-                console.log("app", app.data)
-                res.status(200).json({
-                    appData: app.data
-                })
-            }).catch((E)=>{
-                res.status(400).json({
-                    appData: E
-                }) 
-            })
-        }, 3000);
+        const fileDIr = path.join(__dirname, "../", "resources");
 
-
-        fs.rm(`${fileDIr}/${fileName}`, (err) => {
+        fs.writeFile(`${fileDIr}/${fileName}`, base64, { encoding: "base64" }, async (err) => {
             if (err) {
+                console.log("errr", err)
                 res.status(400).json({
                     err
                 })
                 return
             }
-            console.log("file deleted")
+
+
+            await sendPassport(applicantId, `${fileDIr}/${fileName}`, {
+                country,
+                idDocType: "PASSPORT"
+            }, secretKey, token)
+
+            await fireOCR(applicantId, secretKey, token)
+            setTimeout(async () => {
+                getApplicant(externalUserId, secretKey, token).then((app) => {
+                    console.log("app", app.data)
+                    res.status(200).json({
+                        appData: app.data
+                    })
+                }).catch((E) => {
+                    res.status(400).json({
+                        appData: E
+                    })
+                })
+            }, 3000);
+
+
+            fs.rm(`${fileDIr}/${fileName}`, (err) => {
+                if (err) {
+                    res.status(400).json({
+                        err
+                    })
+                    return
+                }
+                console.log("file deleted")
+            })
         })
-    })
+    } catch (r) {
+        console.log("r", r)
+        res.status(400).json({
+            r
+        })
+    }
+
 
 })
 
